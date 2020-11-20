2d3872d677d8fe4c53459456b756d9e0f1b06669
lnash94 <sumudunissanke@gmail.com>
2020-11-20 16:34:35 +0530

    Add build.gradle changes

--- openapi-validator/src/main/java/module-info.java
+++ openapi-validator/src/main/java/module-info.java
@@ -21,8 +21,8 @@ module io.ballerina.ballerina.openapi.validator {
     requires io.ballerina.lang;
     requires io.ballerina.tools.api;
     requires io.ballerina.packerina;
-//    requires io.ballerina.runtime;
-//    requires io.ballerina.runtime.api;
+    requires io.ballerina.runtime;
+    requires io.ballerina.runtime.api;
 
     exports org.ballerinalang.openapi.validator;
     exports org.ballerinalang.openapi.validator.error;
