c6239a4c1202c475f3a8d95c67490960bb7f2e93
lnash94 <sumudunissanke@gmail.com>
2020-12-15 18:21:48 +0530

    Fix review suggestions

--- openapi-cli/src/main/java/org/ballerinalang/ballerina/ConverterUtils.java
+++ openapi-cli/src/main/java/org/ballerinalang/ballerina/ConverterUtils.java
@@ -1,17 +1,19 @@
 /*
- * Copyright (c) 2018, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *  Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
  */
  
 package org.ballerinalang.ballerina;
@@ -91,6 +93,12 @@ public class ConverterUtils {
 
         return annotation;
     }
+
+    /**
+     * This util function is for converting ballerina type to openapi type.
+     * @param type this string type parameter according to ballerina type
+     * @return  this return the string value of openAPI type
+     */
 
     public static String convertBallerinaTypeToOpenAPIType(String type) {
         String convertedType;
