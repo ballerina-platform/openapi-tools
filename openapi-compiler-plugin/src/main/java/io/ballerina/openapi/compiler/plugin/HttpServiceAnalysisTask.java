04567ec3b4d1cdb3743d52d743538b3754573245
ayeshLK <ayesh9303@gmail.com>
2021-07-28 17:55:39 +0530

    Add basic compiler plugin structure

---
+++ openapi-compiler-plugin/src/main/java/io/ballerina/openapi/compiler/plugin/HttpServiceAnalysisTask.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.ballerina.openapi.compiler.plugin;
+
+import io.ballerina.compiler.api.symbols.*;
+import io.ballerina.compiler.syntax.tree.ServiceDeclarationNode;
+import io.ballerina.projects.plugins.AnalysisTask;
+import io.ballerina.projects.plugins.SyntaxNodeAnalysisContext;
+
+import java.util.Optional;
+
+/**
+ * {@code HttpServiceAnalysisTask} analyses the HTTP service for which the OpenApi doc is generated.
+ */
+public class HttpServiceAnalysisTask implements AnalysisTask<SyntaxNodeAnalysisContext> {
+    private static final String PACKAGE_ORG = "ballerina";
+    private static final String PACKAGE_NAME = "http";
+
+    @Override
+    public void perform(SyntaxNodeAnalysisContext context) {
+        ServiceDeclarationNode serviceNode = (ServiceDeclarationNode) context.node();
+        Optional<Symbol> serviceDeclarationOpt = context.semanticModel().symbol(serviceNode);
+        if (serviceDeclarationOpt.isPresent()) {
+            ServiceDeclarationSymbol serviceDeclarationSymbol = (ServiceDeclarationSymbol) serviceDeclarationOpt.get();
+            if (isHttpService(serviceDeclarationSymbol)) {
+                // todo: put logic to generate open-api doc
+            }
+        }
+    }
+
+    private boolean isHttpService(ServiceDeclarationSymbol serviceSymbol) {
+        return serviceSymbol.listenerTypes().stream().anyMatch(this::isHttpListener);
+    }
+
+    private boolean isHttpListener(TypeSymbol listenerType) {
+        if (listenerType.typeKind() == TypeDescKind.UNION) {
+            return ((UnionTypeSymbol) listenerType).memberTypeDescriptors().stream()
+                    .filter(typeDescriptor -> typeDescriptor instanceof TypeReferenceTypeSymbol)
+                    .map(typeReferenceTypeSymbol -> (TypeReferenceTypeSymbol) typeReferenceTypeSymbol)
+                    .anyMatch(typeReferenceTypeSymbol ->
+                            typeReferenceTypeSymbol.getModule().isPresent()
+                                    && isHttp(typeReferenceTypeSymbol.getModule().get()
+                            ));
+        }
+
+        if (listenerType.typeKind() == TypeDescKind.TYPE_REFERENCE) {
+            Optional<ModuleSymbol> moduleOpt = ((TypeReferenceTypeSymbol) listenerType).typeDescriptor().getModule();
+            return moduleOpt.isPresent() && isHttp(moduleOpt.get());
+        }
+
+        if (listenerType.typeKind() == TypeDescKind.OBJECT) {
+            Optional<ModuleSymbol> moduleOpt = listenerType.getModule();
+            return moduleOpt.isPresent() && isHttp(moduleOpt.get());
+        }
+
+        return false;
+    }
+
+    private boolean isHttp(ModuleSymbol moduleSymbol) {
+        Optional<String> moduleNameOpt = moduleSymbol.getName();
+        return moduleNameOpt.isPresent() && PACKAGE_NAME.equals(moduleNameOpt.get())
+                && PACKAGE_ORG.equals(moduleSymbol.id().orgName());
+    }
+}
