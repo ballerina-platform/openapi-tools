ba65302cbbaea9e8ff4a1d771fe9f8a57881d0a1
ayeshLK <ayesh9303@gmail.com>
2021-08-18 16:23:22 +0530

    Add initial structure for openapi-extension tests

---
+++ openapi-extension-tests/src/test/java/io/ballerina/openapi/extension/OpenApiExtensionTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.ballerina.openapi.extension;
+
+import io.ballerina.projects.DiagnosticResult;
+import io.ballerina.projects.Package;
+import io.ballerina.projects.PackageCompilation;
+import io.ballerina.projects.ProjectEnvironmentBuilder;
+import io.ballerina.projects.directory.BuildProject;
+import io.ballerina.projects.environment.Environment;
+import io.ballerina.projects.environment.EnvironmentBuilder;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * This class includes tests for Ballerina WebSub compiler plugin.
+ */
+public class OpenApiExtensionTest {
+    private static final Path RESOURCE_DIRECTORY = Paths
+            .get("src", "test", "resources", "ballerina_sources").toAbsolutePath();
+    private static final Path DISTRIBUTION_PATH = Paths
+            .get("build", "target", "ballerina-distribution").toAbsolutePath();
+
+    @Test
+    public void testValidServiceDeclaration() {
+        Package currentPackage = loadPackage("sample_1");
+        PackageCompilation compilation = currentPackage.getCompilation();
+        DiagnosticResult diagnosticResult = compilation.diagnosticResult();
+        Assert.assertEquals(diagnosticResult.diagnostics().size(), 0);
+    }
+
+    private Package loadPackage(String path) {
+        Path projectDirPath = RESOURCE_DIRECTORY.resolve(path);
+        BuildProject project = BuildProject.load(getEnvironmentBuilder(), projectDirPath);
+        return project.currentPackage();
+    }
+
+    private static ProjectEnvironmentBuilder getEnvironmentBuilder() {
+        Environment environment = EnvironmentBuilder.getBuilder().setBallerinaHome(DISTRIBUTION_PATH).build();
+        return ProjectEnvironmentBuilder.getBuilder(environment);
+    }
+}
