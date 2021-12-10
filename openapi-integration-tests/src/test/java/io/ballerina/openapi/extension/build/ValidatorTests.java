5be6818f4ba256f98e93dc086ac99b4b45d686a7
lnash94 <sumudunissanke@gmail.com>
2021-12-10 10:21:29 +0530

    Fix review suggestions

--- openapi-integration-tests/src/test/java/io/ballerina/openapi/extension/build/ValidatorTests.java
+++ openapi-integration-tests/src/test/java/io/ballerina/openapi/extension/build/ValidatorTests.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package io.ballerina.openapi.extension.build;
 
 import io.ballerina.openapi.cmd.TestUtil;
@@ -37,8 +54,8 @@ public class ValidatorTests {
         List<String> buildArgs = new LinkedList<>();
         buildArgs.add("project_1");
         InputStream successful = TestUtil.executeOpenapiBuild(DISTRIBUTION_FILE_NAME, TEST_RESOURCE, buildArgs);
-        String msg = " ERROR [service.bal:(4:1,19:2)] Could not find q parameter in the Ballerina service " +
-                "resource for the method get of the path /weather which is documented in the OpenAPI contract";
+        String msg = " ERROR [service.bal:(4:1,19:2)] Missing OpenAPI contract parameter `q` in the counterpart" +
+                " Ballerina service resource (method: `get`, path: `/weather`)";
         try (BufferedReader br = new BufferedReader(new InputStreamReader(successful))) {
             Stream<String> logLines = br.lines();
             String generatedLog = logLines.collect(Collectors.joining(System.lineSeparator()));
