fa95efb7360d4145461d8d6d0767ce09cb0d29d8
lnash94 <sumudunissanke@gmail.com>
2022-01-20 16:54:19 +0530

    Fix checkstyle issue

--- openapi-bal-service/src/main/java/io/ballerina/openapi/converter/service/OpenAPIComponentMapper.java
+++ openapi-bal-service/src/main/java/io/ballerina/openapi/converter/service/OpenAPIComponentMapper.java
@@ -184,7 +184,8 @@ public class OpenAPIComponentMapper {
                         typeReference));
                 schema = components.getSchemas();
             } else if (field.getValue().typeDescriptor().typeKind() == TypeDescKind.UNION) {
-                property = handleUnionType((UnionTypeSymbol) field.getValue().typeDescriptor(), property, componentName);
+                property = handleUnionType((UnionTypeSymbol) field.getValue().typeDescriptor(), property,
+                        componentName);
                 schema = components.getSchemas();
             }
             if (property instanceof ArraySchema) {
@@ -266,6 +267,9 @@ public class OpenAPIComponentMapper {
     }
 
     private boolean isSameRecord(String parentComponentName, TypeReferenceTypeSymbol typeReferenceTypeSymbol) {
+        if (parentComponentName == null) {
+            return false;
+        }
         return typeReferenceTypeSymbol.getName().isPresent() &&
                 parentComponentName.equals(typeReferenceTypeSymbol.getName().get().trim());
     }
