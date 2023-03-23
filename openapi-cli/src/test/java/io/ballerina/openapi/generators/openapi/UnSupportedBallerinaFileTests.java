3283526146cbb857bacf679b87ed762877d3c247
aneeshafedo <aneeshafedo@gmail.com>
2023-03-23 09:06:20 +0530

    Address review suggestions

--- openapi-cli/src/test/java/io/ballerina/openapi/generators/openapi/UnSupportedBallerinaFileTests.java
+++ openapi-cli/src/test/java/io/ballerina/openapi/generators/openapi/UnSupportedBallerinaFileTests.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.ballerina.openapi.generators.openapi;
 
 import io.ballerina.openapi.cmd.OASContractGenerator;
@@ -29,9 +47,21 @@ public class UnSupportedBallerinaFileTests {
         System.setOut(new PrintStream(outputStreamCaptor));
     }
 
-    @Test(description = "Test the warning message when unsupported bal file is given")
-    public void testCompilerWarningForUnsupportedBallerinaFile() {
+    @Test(description = "Test the warning message when unsupported bal file with graphql service is given")
+    public void testCompilerWarningForUnsupportedGraphQLService() {
         Path ballerinaFilePath = RES_DIR.resolve("graphql_service.bal");
+        OASContractGenerator openApiConverter = new OASContractGenerator();
+        openApiConverter.generateOAS3DefinitionsAllService(ballerinaFilePath, tempDir, null
+                , false);
+        List<OpenAPIConverterDiagnostic> errors = openApiConverter.getErrors();
+        Assert.assertFalse(errors.isEmpty());
+        Assert.assertEquals(errors.get(0).getMessage(),
+                "Given Ballerina file does not contain any HTTP service.");
+    }
+
+    @Test(description = "Test the warning message when unsupported bal file with main function is given")
+    public void testCompilerWarningForUnsupportedBallerinaFile() {
+        Path ballerinaFilePath = RES_DIR.resolve("unsupported_main_file.bal");
         OASContractGenerator openApiConverter = new OASContractGenerator();
         openApiConverter.generateOAS3DefinitionsAllService(ballerinaFilePath, tempDir, null
                 , false);
