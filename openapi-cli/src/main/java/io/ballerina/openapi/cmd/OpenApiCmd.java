ee3b865a55b746046e008edd17b5a99f768e571d
aneeshafedo <aneeshafedo@gmail.com>
2023-06-14 09:55:43 +0530

    Add warning when the --without-data-binding is given in the client mode

--- openapi-cli/src/main/java/io/ballerina/openapi/cmd/OpenApiCmd.java
+++ openapi-cli/src/main/java/io/ballerina/openapi/cmd/OpenApiCmd.java
@@ -43,6 +43,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static io.ballerina.openapi.cmd.CmdConstants.BAL_EXTENSION;
+import static io.ballerina.openapi.cmd.CmdConstants.CLIENT;
 import static io.ballerina.openapi.cmd.CmdConstants.JSON_EXTENSION;
 import static io.ballerina.openapi.cmd.CmdConstants.REMOTE;
 import static io.ballerina.openapi.cmd.CmdConstants.RESOURCE;
@@ -184,6 +185,12 @@ public class OpenApiCmd implements BLauncherCmd {
                 if (!clientResourceMode && mode != null && mode.equals(SERVICE)) {
                     // Exit the code generation process
                     outStream.println("'--client-methods' option is only available in client generation mode.");
+                    exitError(this.exitWhenFinish);
+                }
+
+                if (generateWithoutDataBinding && mode != null && mode.equals(CLIENT)) {
+                    // Exit the code generation process
+                    outStream.println("'--without-data-binding' option is only available in service generation mode.");
                     exitError(this.exitWhenFinish);
                 }
                 try {
