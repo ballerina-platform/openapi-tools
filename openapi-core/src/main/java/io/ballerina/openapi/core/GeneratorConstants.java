168b99d7022dee31bf16285c51f2413b93713452
lnash94 <sumudunissanke@gmail.com>
2023-07-05 16:51:55 +0530

    Rename regex constant name

--- openapi-core/src/main/java/io/ballerina/openapi/core/GeneratorConstants.java
+++ openapi-core/src/main/java/io/ballerina/openapi/core/GeneratorConstants.java
@@ -88,8 +88,8 @@ public class GeneratorConstants {
 
     public static final String OAS_PATH_SEPARATOR = "/";
     public static final String ESCAPE_PATTERN = "([\\[\\]\\\\?!<>@#&~'`*\\-=^+();:\\/{}\\s|.$])";
-    public static final String REGEX_INCLUDE_ONLY_LETTERS = "\\b[_a-zA-Z][_a-zA-Z0-9]*\\b";
-    public static final String REGEX_WORDS_STARTING_WITH_NUMBER = "^[0-9].*";
+    public static final String REGEX_WITHOUT_SPECIAL_CHARACTERS = "\\b[_a-zA-Z][_a-zA-Z0-9]*\\b";
+    public static final String REGEX_WORDS_STARTING_WITH_NUMBERS = "^[0-9].*";
     public static final String REGEX_ONLY_NUMBERS_OR_NUMBERS_WITH_SPECIAL_CHARACTERS = "\\b[0-9([\\[\\]\\\\?!<>@#&~'`" +
             "*\\-=^+();:\\/{}\\s|.$])]*\\b";
     //ClientCode generator
