7818dcba9bc2108bb3afce6d2d35c6db3435803f
Aneesha Fernando <34091327+aneeshafedo@users.noreply.github.com>
2023-08-15 21:01:59 +0530

    Update openapi-cli/src/test/java/io/ballerina/openapi/generators/service/ServiceDiagnosticTests.java
    Co-authored-by: Sumudu Nissanka <lnash94@users.noreply.github.com>

--- openapi-cli/src/test/java/io/ballerina/openapi/generators/service/ServiceDiagnosticTests.java
+++ openapi-cli/src/test/java/io/ballerina/openapi/generators/service/ServiceDiagnosticTests.java
@@ -170,7 +170,8 @@ public class ServiceDiagnosticTests {
                 {"vendor_specific_mime_types.yaml"},
 //                {"ballerinax_connector_tests/ably.yaml"}, // HTTP status code '2XX' is not supported in Ballerina
                 {"ballerinax_connector_tests/azure.iot.yaml"},
-                {"ballerinax_connector_tests/beezup.yaml"},
+                //TODO: Uncomment when fixed https://github.com/ballerina-platform/openapi-tools/issues/1440
+//                {"ballerinax_connector_tests/beezup.yaml"},
                 {"ballerinax_connector_tests/files.com.yaml"},
                 {"ballerinax_connector_tests/openweathermap.yaml"},
                 {"ballerinax_connector_tests/soundcloud.yaml"},
