a4d3f8586adf3026a6421d7bfa7d0346a59b67cd
lnash94 <sumudunissanke@gmail.com>
2023-10-13 21:27:44 +0530

    Provide bal pre-build tool support

---
+++ openapi-client-bal-task-plugin/src/main/java/io/ballerina/openapi/bal/tool/client/Constants.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.ballerina.openapi.bal.tool.client;
+
+import io.ballerina.tools.diagnostics.DiagnosticSeverity;
+
+/**
+ * This class includes constants for client IDL generator.
+ *
+ * @since 1.9.0
+ */
+public class Constants {
+    public static final String OPENAPI_CLIENT_REFERENCE = "openapi:ClientConfig";
+    public static final String TAGS = "tags";
+    public static final String OPERATIONS = "operations";
+    public static final String NULLABLE = "nullable";
+    public static final String IS_RESOURCE = "isResource";
+    public static final String LICENSE = "license";
+    public static final String TRUE = "true";
+    public static final String MODULE_ALIAS = "openapi_client";
+    public static final String OPENAPI_REGEX_PATTERN = "\"?(openapi|swagger)\"?\\s*:\\s*\"?[0-9]\\d*\\.\\d+\\.\\d+\"?";
+
+    /**
+     * Enum class for containing diagnostic messages.
+     */
+    public enum DiagnosticMessages {
+        LICENSE_PATH_BLANK("OAS_IDL_CLIENT_01", "given license file path is an empty string.",
+                DiagnosticSeverity.WARNING),
+        ERROR_WHILE_READING_LICENSE_FILE("OAS_IDL_CLIENT_02", "unexpected error occurred while reading the license",
+                DiagnosticSeverity.ERROR),
+        ERROR_WHILE_GENERATING_CLIENT("OAS_IDL_CLIENT_03", "unexpected error occurred while generating the client",
+                                         DiagnosticSeverity.ERROR),
+        PARSER_ERROR("OAS_IDL_CLIENT_04", "", DiagnosticSeverity.ERROR); // parser errors are assigned directly to
+        // diagnostic via exceptions.
+        private final String code;
+        private final String description;
+        private final DiagnosticSeverity severity;
+
+        DiagnosticMessages(String code, String description, DiagnosticSeverity severity) {
+            this.code = code;
+            this.description = description;
+            this.severity = severity;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public DiagnosticSeverity getSeverity() {
+            return severity;
+        }
+    }
+}
---
+++ openapi-client-bal-task-plugin/src/main/java/module-info.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2022, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+module io.ballerina.openapi.idl.client {
+    requires io.ballerina.lang;
+    requires io.ballerina.parser;
+    requires io.ballerina.stdlib.http;
+    requires io.ballerina.tools.api;
+    requires io.ballerina.formatter.core;
+    requires io.ballerina.openapi.converter;
+    requires io.swagger.v3.core;
+    requires io.swagger.v3.oas.models;
+    requires java.ws.rs;
+    requires jsr305;
+    requires org.apache.commons.io;
+    requires org.slf4j;
+    requires org.apache.commons.lang3;
+    requires io.ballerina.openapi.core;
+}
