a9c7016dc2598e21fc0f94c771b4cd816660a635
ayeshLK <ayesh9303@gmail.com>
2024-01-22 09:31:16 +0530

    Update licensing headers and doc-comments

--- ballerina-to-openapi/src/main/java/io/ballerina/openapi/service/mapper/hateoas/Service.java
+++ ballerina-to-openapi/src/main/java/io/ballerina/openapi/service/mapper/hateoas/Service.java
@@ -1,19 +1,19 @@
 /*
- * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com).
+ *  Copyright (c) 2024, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
  *
- * WSO2 Inc. licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License.
- * You may obtain a copy of the License at
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
  */
 
 package io.ballerina.openapi.service.mapper.hateoas;
@@ -23,6 +23,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * A data class to hold service level HATEOAS meta-data.
+ *
+ * @since 1.6.0
+ */
 public class Service {
     private final String packageId;
     private final int serviceId;
