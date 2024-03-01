f6c4fac9dcead757556c6033d65d3816d4ce4142
TharmiganK <ktharmi176@gmail.com>
2024-03-01 16:25:12 +0530

    Fix test failures

--- ballerina-to-openapi/src/main/java/io/ballerina/openapi/service/mapper/interceptor/InterceptorPipeline.java
+++ ballerina-to-openapi/src/main/java/io/ballerina/openapi/service/mapper/interceptor/InterceptorPipeline.java
@@ -81,6 +81,7 @@ public class InterceptorPipeline {
                     pipeline.initResInterceptor = prevInterceptor;
                 }
             }
+            return pipeline;
         }
         return null;
     }
