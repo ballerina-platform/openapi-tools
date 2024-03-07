cc025be88e22cf7bc15e3454e825a7e72c96e8c7
lnash94 <sumudunissanke@gmail.com>
2024-03-07 15:11:46 +0530

    Fix review suggestions

--- openapi-cli/src/main/java/io/ballerina/openapi/cmd/OASContractGenerator.java
+++ openapi-cli/src/main/java/io/ballerina/openapi/cmd/OASContractGenerator.java
@@ -109,7 +109,8 @@ public class OASContractGenerator {
                 .anyMatch(d -> DiagnosticSeverity.ERROR.equals(d.diagnosticInfo().severity()));
         if (hasCompilationErrors || hasErrorsFromCodeGenAndModify) {
             // if there are any compilation errors, do not proceed and those diagnostic will display to user
-            outStream.println("OpenAPI contract generation failed due to Ballerina code has compilation errors. :");
+            outStream.println("openapi contract generation is skipped because the Ballerina file has the" +
+                    " following compilation error(s):");
             compilation.diagnosticResult().diagnostics().forEach(diagnostic -> {
                 outStream.println(diagnostic.toString());
             });
