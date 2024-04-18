1d96344f926ba065451ccaec942ec0f121f80f03
TharmiganK <ktharmi176@gmail.com>
2024-04-18 14:19:37 +0530

    Remove the request param limitation with status code binding

--- openapi-cli/src/main/java/io/ballerina/openapi/cmd/BallerinaCodeGenerator.java
+++ openapi-cli/src/main/java/io/ballerina/openapi/cmd/BallerinaCodeGenerator.java
@@ -442,21 +442,10 @@ public class BallerinaCodeGenerator {
 
     private static BallerinaClientGenerator getBallerinaClientGenerator(OASClientConfig oasClientConfig,
                                                                         boolean statusCodeBinding) {
-        if (!statusCodeBinding || hasRequestBinding(oasClientConfig.getOpenAPI())) {
-            if (statusCodeBinding) {
-                outStream.println("WARNING: the generated client will not have status code response binding since " +
-                        "the OpenAPI definition contains unsupported media-type for request payload binding.");
-            }
+        if (!statusCodeBinding) {
             return new BallerinaClientGenerator(oasClientConfig);
         }
         return new BallerinaClientGeneratorWithStatusCodeBinding(oasClientConfig);
-    }
-
-    private static boolean hasRequestBinding(OpenAPI openAPI) {
-        return openAPI.getPaths().values().stream().anyMatch(pathItem -> pathItem.readOperations().stream()
-                .anyMatch(operation -> operation.getRequestBody() != null &&
-                        operation.getRequestBody().getContent().keySet().stream()
-                                .anyMatch(GeneratorUtils::hasRequestBinding)));
     }
 
 
