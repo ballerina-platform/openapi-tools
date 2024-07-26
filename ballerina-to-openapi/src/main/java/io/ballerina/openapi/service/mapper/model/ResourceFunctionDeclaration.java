e9190c3636978e9aae0db4ea35c08a2fe887e667
TharmiganK <ktharmi176@gmail.com>
2024-07-26 12:39:55 +0530

    Add license headers

--- ballerina-to-openapi/src/main/java/io/ballerina/openapi/service/mapper/model/ResourceFunctionDeclaration.java
+++ ballerina-to-openapi/src/main/java/io/ballerina/openapi/service/mapper/model/ResourceFunctionDeclaration.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (c) 2024, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 LLC. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package io.ballerina.openapi.service.mapper.model;
 
 import io.ballerina.compiler.api.SemanticModel;
@@ -12,6 +29,11 @@ import io.ballerina.tools.diagnostics.Location;
 
 import java.util.Optional;
 
+/**
+ * The {@link ResourceFunctionDeclaration} represents the resource function declaration.
+ *
+ * @since 2.1.0
+ */
 public class ResourceFunctionDeclaration implements ResourceFunction {
     MethodDeclarationNode resourceFunction;
     FunctionSignatureNode functionSignature;
