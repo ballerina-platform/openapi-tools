1e60b55f6b5e84e498ade428ff2581ce46f44990
TharmiganK <ktharmi176@gmail.com>
2025-10-05 10:11:36 +0530

    Add test case for flatten with allOf schema

--- openapi-cli/src/test/java/io/ballerina/openapi/cmd/OpenAPICmdTest.java
+++ openapi-cli/src/test/java/io/ballerina/openapi/cmd/OpenAPICmdTest.java
@@ -928,6 +928,16 @@ public class OpenAPICmdTest extends OpenAPICommandTest {
         compareFiles(expectedFilePath, tmpDir.resolve("flattened_openapi.json"));
     }
 
+    @Test(description = "Test openapi flatten sub command with allOf schema with only one inline schema")
+    public void testFlattenWithAllOf() throws IOException {
+        Path expectedFilePath = resourceDir.resolve(Paths.get("cmd/flatten/flattened_allof_openapi_expected.yaml"));
+        String[] args = {"-i", resourceDir + "/cmd/flatten/allof_openapi.yaml", "-o", tmpDir.toString()};
+        Flatten flatten = new Flatten();
+        new CommandLine(flatten).parseArgs(args);
+        flatten.execute();
+        compareFiles(expectedFilePath, tmpDir.resolve("flattened_openapi.yaml"));
+    }
+
     private void compareFiles(Path expectedFilePath, Path generatedFilePath) throws IOException {
         if (Files.exists(generatedFilePath)) {
             String expectedOpenAPIContent = "";
